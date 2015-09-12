16197be64b99cfdf5bd07a39f1fcfe0f5698f4b2
paladox <thomasmulhall410@yahoo.com>
2015-09-12 14:59:26 +0100

    Convert spaces to tabs in main php file
    Change-Id: I2b4fd3355e0acb471fdeb6ccab99450a0cd9e870

--- strapping.php
+++ strapping.php
@@ -10,11 +10,11 @@
 if( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
  
 $wgExtensionCredits['skin'][] = array(
-        'path' => __FILE__,
-        'name' => 'Strapping',
-        'url' => "https://github.com/OSAS/strapping-mediawiki",
-        'author' => 'Garrett LeSage',
-        'descriptionmsg' => 'strapping-desc',
+	'path' => __FILE__,
+	'name' => 'Strapping',
+	'url' => "https://github.com/OSAS/strapping-mediawiki",
+	'author' => 'Garrett LeSage',
+	'descriptionmsg' => 'strapping-desc',
 );
 
 $wgValidSkinNames['strapping'] = 'Strapping';
@@ -22,19 +22,19 @@ $wgAutoloadClasses['SkinStrapping'] = dirname(__FILE__).'/Strapping.skin.php';
 $wgMessagesDirs['SkinStrapping'] = __DIR__ . '/i18n';
  
 $wgResourceModules['skins.strapping'] = array(
-        'styles' => array(
-                'strapping/bootstrap/css/bootstrap.css' => array( 'media' => 'screen' ),
-                'strapping/bootstrap/css/bootstrap-responsive.css' => array( 'media' => 'screen' ),
-                'strapping/bootstrap/awesome/css/font-awesome.css' => array( 'media' => 'screen' ),
-                'strapping/screen.css' => array( 'media' => 'screen' ),
-                'strapping/theme.css' => array( 'media' => 'screen' ),
+	'styles' => array(
+		'strapping/bootstrap/css/bootstrap.css' => array( 'media' => 'screen' ),
+		'strapping/bootstrap/css/bootstrap-responsive.css' => array( 'media' => 'screen' ),
+		'strapping/bootstrap/awesome/css/font-awesome.css' => array( 'media' => 'screen' ),
+		'strapping/screen.css' => array( 'media' => 'screen' ),
+		'strapping/theme.css' => array( 'media' => 'screen' ),
 	),
 	'scripts' => array(
 		'strapping/bootstrap/js/bootstrap.js',
 		'strapping/strapping.js',
 	),
-        'remoteBasePath' => &$GLOBALS['wgStylePath'],
-        'localBasePath' => &$GLOBALS['wgStyleDirectory'],
+	'remoteBasePath' => &$GLOBALS['wgStylePath'],
+	'localBasePath' => &$GLOBALS['wgStyleDirectory'],
 );
 
 # Default options to customize skin
